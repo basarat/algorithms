5445af6c031ae132bdb5d5bffee3a495f1e3d51f
basarat <basaratali@gmail.com>
2017-04-17 10:10:21 +1000

    permutations ... later

---
+++ src/permutations/permutations.ts
@@ -0,0 +1,21 @@
+function* permutations<T>(permutation: T[]): Iterator<T[]> {
+  var length = permutation.length,
+    c = Array(length).fill(0),
+    i = 1;
+
+  yield permutation;
+  while (i < length) {
+    if (c[i] < i) {
+      var k = (i % 2) ? c[i] : 0,
+        p = permutation[i];
+      permutation[i] = permutation[k];
+      permutation[k] = p;
+      ++c[i];
+      i = 1;
+      yield permutation;
+    } else {
+      c[i] = 0;
+      ++i;
+    }
+  }
+}
