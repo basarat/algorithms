343d38fe5bdbb6b7c6e57bdd0049381d087ca524
basarat <basaratali@gmail.com>
2017-04-17 10:15:52 +1000

    comment

--- src/anagram/anagram.ts
+++ src/anagram/anagram.ts
@@ -1,4 +1,11 @@
-export function areAnagrams(s1: string, s2: string) {
+/**
+ * A word is an anagram of another if you can rearrange its characters to produce the second word.
+ * Given two strings determines if they are anagrams of each other.
+ * - 'earth' and 'heart' are anagrams
+ * - 'silent' and 'listen' are anagrams
+ * - 'foo' and 'bar' are not anagrams
+ */
+export function areAnagrams(s1: string, s2: string): boolean {
   const charCount = new Map<string, number>();
   for (const char of s1.split('')) {
     charCount.set(char, (charCount.get(char) || 0) + 1);
