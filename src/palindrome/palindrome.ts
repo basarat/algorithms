71194091baf8b29dbcc59b32737e27af18d07450
Basarat Ali Syed <Basarat.AliSyed@auspost.com.au>
2017-04-18 13:31:42 +1000

    pal

--- src/palindrome/palindrome.ts
+++ src/palindrome/palindrome.ts
@@ -1,9 +1,22 @@
-export function isPalindrome(str: string) {
+/**
+ * @module Palindrome solver
+ * A palindrome is a string that reads the same forward and backward, for example,
+ * - radar, toot, madam.
+ * - Write a function that checks if a given string is a palindrome.
+ */
+
+/**
+ * Returns true if the string is a palindrome
+ */
+export function isPalindrome(str: string): boolean {
   const reversed = str.split('').reverse().join('');
   return reversed === str;
 }
 
-export function isAnyPermutationPalindrome(str: string) {
+/** 
+ * Return true if any permutation of the string is a palindrome
+ */
+export function isAnyPermutationPalindrome(str: string): boolean {
   const unmatched = new Set<string>();
   str.split('').forEach(char => {
     if (unmatched.has(char)) unmatched.delete(char);
