d1e6a5a6441bd2f44aab632f9f68ec8d1217b8c0
Basarat Ali Syed <basarat@basarat.com>
2017-04-24 16:26:30 +1000

    done

--- src/quickSort/quickSort.ts
+++ src/quickSort/quickSort.ts
@@ -14,22 +14,29 @@ function partition(array: number[], lowIndex: number, highIndex: number): void {
   const length = highIndex - lowIndex;
   if (length <= 1) return;
 
+  /** Randomly select a pivot and move it to the head of the array */
   const pivotIndex = lowIndex + Math.floor((Math.random() * length));
   [array[lowIndex], array[pivotIndex]] = [array[pivotIndex], array[lowIndex]];
 
+  /** The first element is our pivot */
   let pivotRank = lowIndex;
   const pivot = array[lowIndex];
+
+  /** Loop through all the elements, partitioning around the pivot */
   for (let index = lowIndex + 1; index < highIndex; index++) {
     if (array[index] < pivot) {
-      [array[index], array[pivotRank]] = [array[pivotRank], array[index]];
       pivotRank++;
+      [array[index], array[pivotRank]] = [array[pivotRank], array[index]];
     }
   }
+
+  /** Finally put the pivot at its rightfull place */
   [array[pivotRank], array[lowIndex]] = [array[lowIndex], array[pivotRank]];
 
 
+  /** Partition all the elements less than the pivot */
   partition(array, lowIndex, pivotRank);
+
+  /** Partition all the elements more than the pivot */
   partition(array, pivotRank + 1, highIndex);
 }
-
-console.log(quickSort([4, 3, 2, 1]));
