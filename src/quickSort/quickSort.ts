a815cb9224cadf7a5de3acfc67a969fff825a815
Basarat Ali Syed <basarat@basarat.com>
2017-04-24 14:57:31 +1000

    wip

--- src/quickSort/quickSort.ts
+++ src/quickSort/quickSort.ts
@@ -14,4 +14,13 @@ export function quickSort(array: number[]): number[] {
 function partition(array: number[], low: number, high: number) {
   const length = high - low;
   const pivot = Math.floor((Math.random() * length));
+
+  /** TODO */
+  /** Partition around the pivot so that [less]pivot[more] in the original array */
+
+  /**
+   * partition([less], less low, less high)
+   * partition([more], more low, more high)
+   */
+  return array;
 }
