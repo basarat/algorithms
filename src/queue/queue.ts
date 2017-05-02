43f5cb5d6a275ee353426a8de3d938a008b53472
Basarat Ali Syed <basarat@basarat.com>
2017-05-02 14:07:36 +1000

    types

--- src/queue/queue.ts
+++ src/queue/queue.ts
@@ -1,19 +1,19 @@
 class Queue<T> {
   lastDequeuedIndex = 1;
   nextEnqueueIndex = 1;
-  storage = Object.create(null);
+  storage: { [index: number]: T } = Object.create(null);
 
   enqueue(data: T) {
     this.storage[this.nextEnqueueIndex] = data;
     this.nextEnqueueIndex++;
   }
 
-  dequeue() {
+  dequeue(): T | undefined {
     if (this.lastDequeuedIndex !== this.nextEnqueueIndex) {
-      const deletedData = this.storage[this.lastDequeuedIndex];
+      const dequeued = this.storage[this.lastDequeuedIndex];
       delete this.storage[this.lastDequeuedIndex];
       this.lastDequeuedIndex++;
-      return deletedData;
+      return dequeued;
     }
   }
 
