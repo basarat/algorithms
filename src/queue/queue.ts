5bac5eb30f972703ce0a71c3c54bdc3a17668170
Basarat Ali Syed <basarat@basarat.com>
2017-05-02 13:44:05 +1000

    tsify

--- src/queue/queue.ts
+++ src/queue/queue.ts
@@ -1,28 +1,23 @@
-function Queue() {
-  this._oldestIndex = 1;
-  this._newestIndex = 1;
-  this._storage = Object.create(null);
-}
+class Queue<T> {
+  lastDequeuedIndex = 1;
+  nextEnqueueIndex = 1;
+  storage = Object.create(null);
 
-Queue.prototype.size = function() {
-  return this._newestIndex - this._oldestIndex;
-};
-
-Queue.prototype.enqueue = function(data) {
-  this._storage[this._newestIndex] = data;
-  this._newestIndex++;
-};
-
-Queue.prototype.dequeue = function() {
-  var oldestIndex = this._oldestIndex,
-    newestIndex = this._newestIndex,
-    deletedData;
-
-  if (oldestIndex !== newestIndex) {
-    deletedData = this._storage[oldestIndex];
-    delete this._storage[oldestIndex];
-    this._oldestIndex++;
-
-    return deletedData;
+  enqueue(data: T) {
+    this.storage[this.nextEnqueueIndex] = data;
+    this.nextEnqueueIndex++;
   }
-};
+
+  dequeue() {
+    if (this.lastDequeuedIndex !== this.nextEnqueueIndex) {
+      const deletedData = this.storage[this.lastDequeuedIndex];
+      delete this.storage[this.lastDequeuedIndex];
+      this.lastDequeuedIndex++;
+      return deletedData;
+    }
+  }
+
+  size() {
+    return this.nextEnqueueIndex - this.lastDequeuedIndex;
+  }
+}
