e7cc5a7897a0ce9b961709e661215792de9c9765
basarat <basaratali@gmail.com>
2017-07-26 07:38:49 +1000

    more tests

--- src/doublyLinkedList/doublyLinkedList.test.ts
+++ src/doublyLinkedList/doublyLinkedList.test.ts
@@ -13,5 +13,13 @@ test('basic', () => {
   expect(list.dequeue()).toBe(undefined);
   expect(Array.from(list.values())).toMatchObject([]);
   list.add(5);
-  expect(Array.from(list.values())).toMatchObject([5]);
+  list.add(10);
+  list.add(1);
+  expect(Array.from(list.values())).toMatchObject([5, 10, 1]);
+  expect(list.pop()).toBe(1);
+  expect(list.dequeue()).toBe(5);
+  expect(list.pop()).toBe(10);
+  expect(list.pop()).toBe(undefined);
+  expect(list.dequeue()).toBe(undefined);
+  expect(Array.from(list.values())).toMatchObject([]);
 });
