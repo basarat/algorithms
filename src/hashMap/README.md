534dbc2d0eb5798ac4227cf610a7142127e4dae1
basarat <basaratali@gmail.com>
2017-07-27 12:24:46 +1000

    hashmap

---
+++ src/hashMap/README.md
@@ -0,0 +1,4 @@
+# Create a JavaScript hashmap
+> A common interview question to weed out beginner / true expert JavaScript developers is to simply ask them to create a dictionary / hashmap / map in ES5.
+> In this lesson we dicuss why `{}` is the wrong way to do it.
+
---
+++ src/hashMap/hashMap.ts
@@ -0,0 +1,32 @@
+/**
+ * A simple JavaScript hashmap
+ */
+export class HashMap<Key extends number | string, Value>{
+  private data: {
+    [key: string]: Value
+  } = Object.create(null);
+
+  /** Gets the value against the key (if any) */
+  get(key: Key): Value | undefined {
+    return this.data[key.toString()];
+  }
+
+  /** Stores the value against the key */
+  set(key: Key, value: Value) {
+    this.data[key.toString()] = value;
+  }
+
+  /** Removes the key/value pair */
+  delete(key: Key) {
+    delete this.data[key.toString()];
+  }
+
+  /**
+   * Returns
+   * - true if there is a value stored against the key
+   * - false if there is no value stored against the key
+   **/
+  has(key: Key): boolean {
+    return typeof this.data[key.toString()] === undefined;
+  }
+}
