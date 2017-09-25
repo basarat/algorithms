ac62017b41e7455c2b94c4c404b8c4465f11bc83
basarat <basaratali@gmail.com>
2017-09-26 09:36:58 +1000

    rank maintainance

---
+++ src/rankMaintainance/rankMaintainance.test.ts
@@ -0,0 +1,15 @@
+import { Minimum } from './rankMaintainance';
+
+test('Minimum', () => {
+  const collection = new Minimum();
+  collection.add(5);
+  collection.add(4);
+  collection.add(1);
+  collection.add(2);
+
+  expect(collection.extract()).toBe(1);
+  expect(collection.extract()).toBe(2);
+  expect(collection.extract()).toBe(4);
+  expect(collection.extract()).toBe(5);
+  expect(collection.extract()).toBe(undefined);
+});
---
+++ src/rankMaintainance/rankMaintainance.ts
@@ -0,0 +1,20 @@
+/**
+ * Maintains a minimum value
+ */
+export class Minimum {
+  data: number[] = [];
+
+  add(item: number) {
+    for (let index = 0; index < this.data.length; index++) {
+      if (this.data[index] < item) {
+        this.data.splice(index, 0, item);
+        return;
+      }
+    }
+    this.data.push(item);
+  }
+
+  extract(): number | undefined {
+    return this.data.pop();
+  }
+}
